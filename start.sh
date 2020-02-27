4cc51e7a0cd6d8d5d37cf2894986fee3697b6824
Victor Jimenez <vic.jicama@gmail.com>
2020-02-27 18:24:47 +0100

    Sync to latest version

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 export LOCAL_TARGET_SERVER_PORT=25000
 export LOCAL_WORKSPACE=./
 
-export LOCAL_VERSION_BOOTSTRAP=1.70.13-initial-server-prod #module @nebulario/tunnel-server-boot-graph
-export LOCAL_VERSION_GRAPH=1.70.17-initial-server-prod #module repoflow/tunnel-server-graph-container
-export LOCAL_VERSION_WORKER=1.70.4-initial-server-prod #module repoflow/tunnel-server-worker-graph-container
+export LOCAL_VERSION_BOOTSTRAP=1.70.0-master #module @nebulario/tunnel-server-boot-graph
+export LOCAL_VERSION_GRAPH=1.70.0-master #module repoflow/tunnel-server-graph-container
+export LOCAL_VERSION_WORKER=1.70.0-master #module repoflow/tunnel-server-worker-graph-container
 
 echo "Starting..."
 echo "{\"dependencies\":{\"@nebulario/tunnel-server-boot-graph\":\"${LOCAL_VERSION_BOOTSTRAP}\"}}" > package.json
